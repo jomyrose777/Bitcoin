2bd904046806d87da69bdd552de5847e8f3fcad2
jomyrose777 <131293379+jomyrose777@users.noreply.github.com>
2024-08-09 23:38:39 -0400

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,9 +191,9 @@ def generate_perpetual_options_decision(indicators, moving_averages):
     sell_signals = [value for key, value in signals.items() if value == 'Sell']
     
     if len(buy_signals) > len(sell_signals):
-        decision = 'Go Long'
-    elif len(sell_signals) > len(buy_signals):
         decision = 'Go Short'
+    elif len(sell_signals) > len(buy_signals):
+        decision = 'Go Long'
     else:
         decision = 'Neutral'
     
